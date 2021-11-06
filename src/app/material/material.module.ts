0780d6f8cf3c934400238b611061a29e213242b4
covuworie <chuk.ovuworie@gmail.com>
2021-11-06 15:01:16 -0500

    Change to favorite and moon icons from material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 const material = [
+  MatBadgeModule,
   MatCardModule,
   MatIconModule,
   MatListModule,
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  faBars,
-  faHeart,
-  faMoon,
-  faSun,
-} from '@fortawesome/free-solid-svg-icons';
 import { Observable } from 'rxjs';
 import HeaderViewModel from './header-view.model';
 import * as UserSettingsActions from '../../store/actions/user-settings.actions';
@@ -19,10 +13,6 @@ import { HeaderFacadeService } from 'src/app/store/facades/header-facade.service
 })
 export class HeaderComponent implements OnInit {
   // public properties
-  faBars = faBars;
-  faHeart = faHeart;
-  faMoon = faMoon;
-  faSun = faSun;
   vm$: Observable<HeaderViewModel> = new Observable();
 
   // public methods
