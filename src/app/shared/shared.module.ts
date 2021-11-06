99ce2ff24d1ffe8376159cc6c5e0880cc2dfa97a
covuworie <chuk.ovuworie@gmail.com>
2021-11-06 16:17:57 -0500

    Remove fortawesome

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CountrySummaryComponent } from './country-summary/country-summary.component';
 import { RouterModule } from '@angular/router';
 import { HyphenateUriPipe } from './hypenate-uri.pipe';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CountryGridComponent } from './country-grid/country-grid.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -17,12 +16,7 @@ import { MaterialModule } from '../material/material.module';
     HeaderComponent,
     NotFoundComponent,
   ],
-  imports: [CommonModule, RouterModule, FontAwesomeModule, MaterialModule],
-  exports: [
-    CountryGridComponent,
-    HyphenateUriPipe,
-    FontAwesomeModule,
-    HeaderComponent,
-  ],
+  imports: [CommonModule, RouterModule, MaterialModule],
+  exports: [CountryGridComponent, HyphenateUriPipe, HeaderComponent],
 })
 export class SharedModule {}
