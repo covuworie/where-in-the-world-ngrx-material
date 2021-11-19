eee20b9981dd8c5677db962846a720f5975da6f4
covuworie <chuk.ovuworie@gmail.com>
2021-11-19 14:27:37 -0600

    Use mat-toolbar for header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 const material = [
@@ -18,6 +19,7 @@ const material = [
   MatInputModule,
   MatListModule,
   MatSelectModule,
+  MatToolbarModule,
   MatTooltipModule,
 ];
 
